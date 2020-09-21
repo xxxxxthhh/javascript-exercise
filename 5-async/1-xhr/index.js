9ed03610bf662a3ca341648cb6fd1bff48df94fa
KyleXu <kyle_x@foxmail.com>
2020-09-21 23:13:54 +0800

    [XTH] fininshed 5-1 xhr

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,19 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
+  xhr.open('Get', url);
+  xhr.send();
 
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) {
+      errorCallback(xhr.statusText);
+    }
+    if (xhr.status >= 200 && xhr.status < 300) {
+      successCallback(xhr.statusText);
+    } else {
+      errorCallback(xhr.statusText);
+    }
+  };
   // end -->
 }
 
