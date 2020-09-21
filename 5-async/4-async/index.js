69e9ab9120414c6536d37272c27e549c5e2f2a10
KyleXu <kyle_x@foxmail.com>
2020-09-21 23:47:51 +0800

    [XTH] fininshed 5-4 async

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,6 +1,8 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
+  const result = await fetch(url, { method: 'GET' }).then(res => res.json());
+  document.writeln(result.name);
   // end -->
 }
 
